2e35d4df5347210b37b600d8506855c6534d1c2d
Sadman Adib <sadmanadib67@gmail.com>
2022-07-31 15:17:23 +0600

    Created Move Object

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -13,6 +13,9 @@ struct ContentView: View {
                    GridItem(.flexible()),
                    GridItem(.flexible())]
     
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumanTurn = true
+    
     var body: some View {
         GeometryReader{ geometry in
             VStack {
@@ -27,10 +30,14 @@ struct ContentView: View {
                                 .opacity(0.5)
                                 .frame(width: geometry.size.width/3 - 15, height: geometry.size.width/3 - 15)
                             
-                            Image(systemName: "xmark")
+                            Image(systemName: moves[item]?.indicator ?? "")
                                 .resizable()
                                 .frame(width: 40, height: 40)
                                 .foregroundColor(.white)
+                        }
+                        .onTapGesture {
+                            moves[item] = Move(player: isHumanTurn ? .human : .computer, boardIndex: item)
+                            isHumanTurn.toggle()
                         }
                     }
                 }
@@ -40,6 +47,19 @@ struct ContentView: View {
             .padding()
 
         }
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
     }
 }
 
