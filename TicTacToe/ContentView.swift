5134c470701fb2e944342dadec34f5b67f516a13
Sadman Adib <sadmanadib67@gmail.com>
2022-07-31 13:47:46 +0600

    GameBoardUI Done

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let columns = [GridItem(.flexible()),
+                   GridItem(.flexible()),
+                   GridItem(.flexible())]
+    
     var body: some View {
-        Text("Hello, world!")
+        GeometryReader{ geometry in
+            VStack {
+                Spacer()
+                
+                LazyVGrid(columns: columns, spacing: 5){
+                    
+                    ForEach(0..<9){ item in
+                        ZStack{
+                            Circle()
+                                .foregroundColor(.red)
+                                .opacity(0.5)
+                                .frame(width: geometry.size.width/3 - 15, height: geometry.size.width/3 - 15)
+                            
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
+                
+                Spacer()
+            }
             .padding()
+
+        }
     }
 }
 
